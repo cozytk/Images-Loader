dea2dc18d874af20bb2722b79ea3b3b4ecc44552
Taekyun Kim <taekkim@student.42seoul.kr>
2023-03-01 15:49:52 +0900

    🎉 Begin a project

---
+++ Images-Loader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Images-Loader
+//
+//  Created by taekkim on 2023/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
